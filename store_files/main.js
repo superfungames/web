592542307cdfdf5e845a8ae1bbb8f2d6af4cbdc1
Gokhan Nas <gokhannas@biggergames.net>
2019-11-28 17:45:05 +0300

    - store sc pixel added

--- store_files/main.js
+++ store_files/main.js
@@ -57,6 +57,60 @@ $(function () {
     fbq('track', 'Lead');
     window.localStorage.setItem(fbPixelId, Date.now());
     location.href = red_URL;
+  })
+  
+  $(".btn-install-sc").on("click", function (e) {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    var currentOs = "PC";
+    if (/windows phone/i.test(userAgent)) {
+        currentOs = "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+      currentOs = "Android";
+    }
+
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      currentOs = "iOS";
+    }
+
+    if(currentOs == "PC")
+      location.href = "https://biggergames.net";
+
+    
+
+    var camp_ios = "https://track.tenjin.io/v0/click/cuhMk6jWxJ9vxlcYxb6tl3";
+    var camp_a_ios = "https://track.tenjin.io/v0/click/dIYPvW4kE4N5c71N2GLcjJ";
+    var camp_b_ios = "https://track.tenjin.io/v0/click/doeGJEuZNBMNQyUQkKl5u3";
+    var camp_c_ios = "https://track.tenjin.io/v0/click/dM59ughYbjzE3reDeHDQAH";
+
+    var params = {};
+    location.search.substr(1).split("&").forEach(function(item) { params[item.split("=")[0]] = item.split("=")[1] });
+    var utm_source = params.site_source_name;
+    //var utm_medium = params.placement;
+    var utm_campaign = params.name;
+    var ad_id = params.adid;
+    var site_id = "BiggerGitSC";
+    var os = params.os;
+    var campType = params.camp;
+    
+    var _camp = "";
+    if(campType == "a")       { _camp = camp_a_ios; }    
+    else if(campType == "b")  { _camp = camp_b_ios; }    
+    else if(campType == "c")  { _camp = camp_c_ios; }    
+    else                      { _camp = camp_ios; }
+    
+    var red_URL = `${_camp}?advertising_id=${ad_id}&site_id=${site_id}&ad_network=${utm_source}&campaign_name=${utm_campaign}`;
+
+    var register = window.localStorage.getItem(pixelSent);
+    if (register == false) {
+      snaptr('track', 'VIEW_CONTENT');
+      window.localStorage.setItem(pixelSent, true);
+    }
+
+    location.href = red_URL;
+    
 	})
 
 	var currentScrollPos = window.pageYOffset;
